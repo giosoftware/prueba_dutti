7cb7f264b1917ea62d41a0b5ca2600bb4fef9159
Sergio Martín <giosoftware@gmail.com>
2019-01-25 18:01:34 +0100

    2.1 y 2.2 Se ha creado un componente customMdList que recibe los datos del controlador padre e imprime en una lista los nombres de cada buque.

--- src/starships/starships.controller.js
+++ src/starships/starships.controller.js
@@ -2,10 +2,36 @@
     'use strict';
     angular
         .module('app')
-        .controller('StarshipsController', StarshipsController);
+        .controller('StarshipsController', StarshipsController)
+        .component('customMdList', {
+            template: '<ul>' +
+                '<li ng-repeat="ship in $ctrl.allShips">' +
+                '<span>{{ship.name}}</span>' +
+                '</li>' +
+                '</ul>',
+            controller: StarshipsController
+        });
 
     StarshipsController.$inject = ['ShipService'];
     function StarshipsController(ShipService) {
+        var vm = this;
+        vm.allShips = [];
+
+        initController();
+
+        function initController() {
+            loadAllShips();
+        }
+
+        function loadAllShips() {
+            ShipService.GetAll()
+                .then(function (ships) {
+                    vm.allShips = ships.results;
+                })
+                .catch(function (err) {
+                    console.error(err.message)
+                });
+        }
 
     }
 
