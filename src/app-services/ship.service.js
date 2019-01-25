980ec581979512e9e0fad866368ee862b7da9de7
Sergio Martín <giosoftware@gmail.com>
2019-01-25 17:51:34 +0100

    1.2, 1.3 y 1.4. El servicio hace una llamada a la URL indicada utilizando la cabecera Autorization none, y muestra el resultado por la consola. Se utilizan promesas para gestionar el resultado.

--- src/app-services/ship.service.js
+++ src/app-services/ship.service.js
@@ -7,7 +7,30 @@
 
     ShipService.$inject = ['$http'];
     function ShipService($http) {
-        
+        var service = {};
+
+        service.GetAll = GetAll;
+
+        return service;
+
+        function GetAll() {
+            return $http.get('https://swapi.co/api/starships/', {
+                headers: { 'Authorization': 'none' }
+            }).then(handleSuccess, handleError('Error getting all users'));
+        }
+
+        // private functions
+
+        function handleSuccess(res) {
+            console.log(res.data)
+            return res.data;
+        }
+
+        function handleError(error) {
+            return function () {
+                return { success: false, message: error };
+            };
+        }
     }
 
 })();
